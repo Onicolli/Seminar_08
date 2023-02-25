6d168473a5bf3c04d5ee329a298338d15475d1fc
onicolli <onicolli>
2023-02-25 10:22:44 +0600

    HomeWork_02

---
+++ Task_02/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+Console.Clear();
+int[,] array = new int[5, 5];
+
+FillArray();
+PrintArray();
+MinValueArray();
+
+void FillArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+        array[i, j] = new Random().Next(1, 10);
+        }
+    }   
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+        Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MinValueArray()
+{
+    int minSum = int.MaxValue;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int rowSum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            rowSum += array[i, j];
+        }
+        if (rowSum < minSum)
+        {
+            minSum = rowSum;
+        }
+    }
+Console.WriteLine($"Минимальная сумма чисел: {minSum}");
+}
+
+
+
